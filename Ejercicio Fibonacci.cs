660bdf99fd4316b8693de414f147787d59d94db4
ArmandoMR99 <armandorua999@gmail.com>
2025-02-09 21:52:10 -0500

    Update Ejercicio Fibonacci.cs

--- Ejercicio Fibonacci.cs
+++ Ejercicio Fibonacci.cs
@@ -1,1 +1,36 @@
-hbuybuh
+namespace FibonacciPrimos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Números Primos en Fibonacci ===");
+            Console.Write("Ingrese cuántos términos desea ver: ");
+            int n = int.Parse(Console.ReadLine());
+
+            MostrarPrimosFibonacci(n);
+        }
+
+        static void MostrarPrimosFibonacci(int n)
+        {
+            int a = 0, b = 1, temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (EsPrimo(a)) Console.WriteLine(a);
+
+                temp = a + b;
+                a = b;
+                b = temp;
+            }
+        }
+
+        static bool EsPrimo(int num)
+        {
+            if (num < 2) return false;
+            for (int i = 2; i * i <= num; i++)
+                if (num % i == 0) return false;
+            return true;
+        }
+    }
+}
