cad84f32ab63fb4795e9abcf0e5eef11275719ee
ArmandoMR99 <armandorua999@gmail.com>
2025-02-09 22:25:54 -0500

    Update Conversor Tiempo.cs

--- Conversor Tiempo.cs
+++ Conversor Tiempo.cs
@@ -1,1 +1,24 @@
-wecgec
+namespace ConversorTiempo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Conversor de Segundos a HH:MM:SS ===");
+            Console.Write("Ingrese la cantidad de segundos: ");
+            int segundos = int.Parse(Console.ReadLine());
+
+            string formatoTiempo = ConvertirSegundos(segundos);
+            Console.WriteLine("Tiempo en formato HH:MM:SS -> "+ formatoTiempo);
+        }
+
+        static string ConvertirSegundos(int segundos)
+        {
+            int horas = segundos / 3600;
+            int minutos = (segundos % 3600) / 60;
+            int segRestantes = segundos % 60;
+
+            return $"{horas:D2}:{minutos:D2}:{segRestantes:D2}";
+        }
+    }
+}
